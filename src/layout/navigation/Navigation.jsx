7a6cb0485a1ff866fa6d8b3ad82e0f63d8f61e1d
IvanGML <somovlife@gmail.com>
2019-03-29 17:35:53 +0300

    improve navigation menu with animated button

--- src/layout/navigation/Navigation.jsx
+++ src/layout/navigation/Navigation.jsx
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import { CSSTransition } from 'react-transition-group';
+import Button from '../../components/button/Button';
 import './navigation.css';
 
-const Navigation = () => (
-  <div className="navigation">
-    <div className="logoWrapper">
-      <img src="https://www.vectorlogo.zone/logos/reactjs/reactjs-card.png" height="60" alt="logo" />
-    </div>
-    <nav>
-      <ul className="nav">
-        <li><NavLink exact to="/">Home</NavLink></li>
-        {/* <li><NavLink to="/button">Button</NavLink></li> */}
-      </ul>
-    </nav>
-  </div>
-);
+class Navigation extends Component {
+  state = {
+    showMenu: false
+  }
+  toggleMenu = () => this.setState((prevState) => ({showMenu: !prevState.showMenu}));
+  render() {
+    const { showMenu } = this.state;
+    return (
+      <div className="navigation">
+        <Button className="transparent-btn" onClick={this.toggleMenu}>Show all components</Button>
+        <div className="logoWrapper">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png" height="60" alt="logo" />
+        </div>
+        <CSSTransition
+          in={showMenu}
+          timeout={400}
+          classNames="menu"
+          unmountOnExit
+          appear
+        >
+          <nav>
+            <ul className="nav">
+              <li><NavLink onClick={this.toggleMenu} exact to="/">Home</NavLink></li>
+              <li><NavLink onClick={this.toggleMenu} to="/button">Button</NavLink></li>
+            </ul>
+          </nav>
+        </CSSTransition>
+      </div>
+    );
+  }
+};
 
 export default Navigation;
