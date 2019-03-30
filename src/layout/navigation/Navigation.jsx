789eb0a7ffb6fe442ffed2c6a6da248fc7adc0a2
IvanGML <somovlife@gmail.com>
2019-03-30 17:57:54 +0300

    add click-outside function to nav menu

--- src/layout/navigation/Navigation.jsx
+++ src/layout/navigation/Navigation.jsx
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import { CSSTransition } from 'react-transition-group';
 import Button from '../../components/button/Button';
+import OutsideClickHandler from 'react-outside-click-handler';
 import './navigation.css';
 
 class Navigation extends Component {
   state = {
     showMenu: false
   }
-  toggleMenu = () => this.setState((prevState) => ({showMenu: !prevState.showMenu}));
+  toggleMenu = () => this.setState((prevState) => ({ showMenu: !prevState.showMenu }));
+  isOpen = () => this.state.showMenu ? this.toggleMenu() : null;
   render() {
     const { showMenu } = this.state;
     return (
@@ -17,20 +19,22 @@ class Navigation extends Component {
         <div className="logoWrapper">
           <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png" height="60" alt="logo" />
         </div>
-        <CSSTransition
-          in={showMenu}
-          timeout={400}
-          classNames="menu"
-          unmountOnExit
-          appear
-        >
-          <nav>
-            <ul className="nav">
-              <li><NavLink onClick={this.toggleMenu} exact to="/">Home</NavLink></li>
-              <li><NavLink onClick={this.toggleMenu} to="/button">Button</NavLink></li>
-            </ul>
-          </nav>
-        </CSSTransition>
+        <OutsideClickHandler onOutsideClick={this.isOpen}>
+          <CSSTransition
+            in={showMenu}
+            timeout={400}
+            classNames="menu"
+            unmountOnExit
+            appear
+          >
+            <nav>
+              <ul className="nav">
+                <li><NavLink onClick={this.toggleMenu} exact to="/">Home</NavLink></li>
+                <li><NavLink onClick={this.toggleMenu} to="/button">Button</NavLink></li>
+              </ul>
+            </nav>
+          </CSSTransition>
+        </OutsideClickHandler>
       </div>
     );
   }
