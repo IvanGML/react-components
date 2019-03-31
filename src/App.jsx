2ceef1bcb47cc798867b104aeff8791f19927683
IvanGML <somovlife@gmail.com>
2019-03-31 19:13:37 +0300

    add animation between pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,6 @@
 import React from 'react';
-
-import Navigation from './layout/navigation/Navigation';
 import Main from './layout/main/Main';
 
-const App = ({ children }) => [
-  <Navigation key="navigation" />,
-  <Main key="main">
-    {children}
-  </Main>,
-];
+const App = ({ children }) => <Main key="main">{children}</Main>;
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import Navigation from './layout/navigation/Navigation';
 import App from './App';
 import Button from './components/button/ButtonSandbox';
 import ButtonGroup from './components/buttonGroup/ButtonGroupSandbox';
@@ -9,16 +11,35 @@ import * as serviceWorker from './serviceWorker';
 
 import Home from './layout/home/Home';
 
+const AppWithTransition = props => {
+  return (
+    <Fragment>
+      <Navigation key="navigation" />,
+      <TransitionGroup>
+        <CSSTransition
+          key={props.location.key}
+          classNames="swipe"
+          timeout={500}
+        >
+          <div className="swipe-container">
+            <App>
+              <Switch location={props.location}>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/button" component={Button} />
+                <Route exact path="/group-button" component={ButtonGroup} />
+                <Redirect to="/" />
+              </Switch>
+            </App>
+          </div>
+        </CSSTransition>
+      </TransitionGroup>
+    </Fragment>
+  );
+}
+
 ReactDOM.render((
   <BrowserRouter>
-    <App>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/button" component={Button} />
-        <Route exact path="/group-button" component={ButtonGroup} />
-        <Redirect to="/" />
-      </Switch>
-    </App>
+    <Route component={AppWithTransition} />
   </BrowserRouter>
 ), document.getElementById('root'));
 
