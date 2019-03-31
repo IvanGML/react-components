ce15f09f4490a0fe2bafb108148c58e1bd30e446
IvanGML <somovlife@gmail.com>
2019-03-31 15:32:52 +0300

    improve button group component

--- src/components/buttonGroup/ButtonGroup.jsx
+++ src/components/buttonGroup/ButtonGroup.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
+import './buttonGroup.css';
 
 const ButtonGroup = ({ children, vertical, className, ...attrs }) => {
     const classes = cx(
